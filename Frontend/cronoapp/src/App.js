8c356beee30358257b4b3ce5f75bd4cf302c20d5
Mohd Haroon Qureshi <qureshiharoon010203@gmail.com>
2022-07-20 19:37:07 +0530

    fp01_033 day2 1st committ

--- Frontend/cronoapp/src/App.js
+++ Frontend/cronoapp/src/App.js
@@ -1,11 +1,9 @@
 import React from "react";
 import {AllRoutes} from "./AllRoutes/AllRoutes"
-import {UserMainPage} from "./Component/userdashboard/Mainpage"
 
 function App() {
   return (
     <div className="App" style={{backgroundColor:"white"}}>
-      {/* <UserMainPage/> */}
       <AllRoutes/>
     </div>
   );
