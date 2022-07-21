46740efa36f26e3a03ee26c994dd568d51e40923
K PRABINA CHOUDHURY <99638485+Prabin8144@users.noreply.github.com>
2022-07-21 12:35:56 +0530

    ok

--- Frontend/cronoapp/src/AllRoutes/AllRoutes.jsx
+++ Frontend/cronoapp/src/AllRoutes/AllRoutes.jsx
@@ -8,9 +8,7 @@ import Login from "../components/Login";
 import Support from "../components/Support";
 import {UserMainPage} from "../Component/userdashboard/Mainpage"
 import {Diary} from "../Component/userdashboard/Diary";
-import { Routes, Route } from 'react-router-dom';
 import { Home } from '../Login/Home';
-import Login from '../Login/Login';
 import Signup from '../Login/Signup';
 
 export const AllRoutes = () => {
