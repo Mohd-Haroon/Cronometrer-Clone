e86edd8bb4e0c7f09f9ae5cd3e8028e05f34b848
Sumit Beniwal <beniwalsumit1412@gmail.com>
2022-07-22 11:34:05 +0530

    complete frontend settings page

--- Frontend/cronoapp/src/Component/userdashboard/Settings.jsx
+++ Frontend/cronoapp/src/Component/userdashboard/Settings.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Button, Input, Switch, Text } from "@chakra-ui/react";
 import styles from "../../CssComponent/Settings.module.css";
+import { BsFillCloudArrowDownFill } from "react-icons/bs";
+import { ImBin } from "react-icons/im";
 
 export const Settings = () => {
   return (
@@ -63,9 +65,41 @@ export const Settings = () => {
             <Switch marginLeft="4%" size="sm" />
           </Text>
         </div>
-        <Button colorScheme="teal" size="sm">
+        <Button
+          backgroundColor="rgb(255,118,63)"
+          className={styles.logoutBtn}
+          colorScheme="teal"
+          size="sm"
+        >
           Logout
         </Button>
+      </div>
+      <div className={styles.settingsChild}>
+        <Text fontWeight="500" fontSize="lg">
+          Account Data
+        </Text>
+      </div>
+      <div className={styles.deleteBox}>
+        <BsFillCloudArrowDownFill  width="20%" fontSize="18px" />
+        <div style={{ width: "50%" }}>
+          <Text fontSize="sm" fontWeight="700">
+            Export Data
+          </Text>
+          <Text fontSize="sm">Export all of your diary data to .csv files</Text>
+        </div>
+        <Button width="30%" variant="ghost" color="rgb(255,118,63)" >Export Data</Button>
+      </div>
+      <div className={styles.deleteBox}>
+        <ImBin fontSize="18px" width="20%" />
+        <div style={{ width: "50%" }}>
+          <Text fontSize="sm" fontWeight="700">
+            Delete Account
+          </Text>
+          <Text fontSize="sm">
+            Permanently delete your account and all of your data.
+          </Text>
+        </div>
+        <Button width="30%" outline="1px solid rgb(255,118,63)" color="rgb(255,118,63)" variant="outline">Delete Account</Button>
       </div>
     </div>
   );
